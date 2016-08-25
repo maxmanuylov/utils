0a1739ba770d3e7a46ffacb7117a4f7113bbdf26
maxim.manuylov <maxim.manuylov@gmail.com>
2016-08-25 03:22:41 +0300

    tls transport utils

---
+++ tls/transport/transport.go
@@ -0,0 +1,37 @@
+package tls_transport
+
+import (
+    "crypto/tls"
+    "crypto/x509"
+    "errors"
+    "net/http"
+)
+
+func New(caCert, clientCert, clientKey []byte) (*http.Transport, error) {
+    tlsConfig, err := newTLSConfig(caCert, clientCert, clientKey)
+    if err != nil {
+        return nil, err
+    }
+
+    transport := http.DefaultTransport.(*http.Transport)
+    transport.TLSClientConfig = tlsConfig
+
+    return &transport, nil
+}
+
+func newTLSConfig(caCert, clientCert, clientKey []byte) (*tls.Config, error) {
+    caPool := x509.NewCertPool()
+    if !caPool.AppendCertsFromPEM(caCert) {
+        return nil, errors.New("No CA certificate found")
+    }
+
+    certificate, err := tls.X509KeyPair(clientCert, clientKey)
+    if err != nil {
+        return nil, err
+    }
+
+    return &tls.Config{
+        Certificates: []tls.Certificate{certificate},
+        RootCAs: caPool,
+    }, nil
+}
