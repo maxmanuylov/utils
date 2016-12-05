b35ee840d99b13f8a72285a9663f8adf4147ea35
maxim.manuylov <maxim.manuylov@gmail.com>
2016-12-05 23:09:51 +0300

    unix socket transport

--- http/transport/transport.go
+++ http/transport/transport.go
@@ -1,6 +1,7 @@
 package http_transport
 
 import (
+    "net"
     "net/http"
 )
 
@@ -8,4 +9,16 @@ func NewDefault() *http.Transport {
     defaultTransport := &http.Transport{}
     *defaultTransport = *http.DefaultTransport.(*http.Transport) // copying
     return defaultTransport
+}
+
+func NewUnix(socketFile string) *http.Transport {
+    transport := NewDefault()
+
+    originalDial := transport.Dial
+
+    transport.Dial = func(_, _ string) (net.Conn, error) {
+        return originalDial("unix", socketFile)
+    }
+
+    return transport
 }
