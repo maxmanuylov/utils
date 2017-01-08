294f115c2937eddf0ff34b2000d2c00e7ad389f5
maxim.manuylov <maxim.manuylov@gmail.com>
2017-01-09 00:49:39 +0300

    improve termination utils

---
+++ application/termination.go
@@ -0,0 +1,38 @@
+package application
+
+import (
+    "os"
+    "os/signal"
+    "syscall"
+)
+
+type TerminationWatcher struct {
+    C <-chan os.Signal
+    c chan os.Signal
+}
+
+func NewTerminationWatcher() *TerminationWatcher {
+    return NewTerminationWatcherN(1)
+}
+
+func NewTerminationWatcherN(n int) *TerminationWatcher {
+    signalsChannel := make(chan os.Signal, n)
+    notifyOnTermination(signalsChannel)
+    return &TerminationWatcher{
+        C: signalsChannel,
+        c: signalsChannel,
+    }
+}
+
+func (tw *TerminationWatcher) Stop() {
+    signal.Stop(tw.c)
+    close(tw.c)
+}
+
+func WaitForTermination() {
+    <-NewTerminationWatcher().C
+}
+
+func notifyOnTermination(c chan<- os.Signal) {
+    signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+}
