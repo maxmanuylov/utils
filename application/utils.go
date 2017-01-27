4f93c4a7a9e685f0026fe01104e31608d98d2318
maxim.manuylov <maxim.manuylov@gmail.com>
2017-01-27 18:25:55 +0300

    switch utility

--- application/utils.go
+++ application/utils.go
@@ -11,6 +11,24 @@ func Run(app func() error) {
     }
 }
 
+func Switch(apps map[string]func()) {
+    if len(os.Args) < 2 {
+        Exit("Application is not specified")
+    }
+
+    appName := os.Args[1]
+    os.Args = append(os.Args[:1], os.Args[2:]...)
+
+    app, ok := apps[appName]
+    if !ok {
+        Exit(fmt.Sprintf("Unknown application: %s", appName))
+    }
+
+	DumpStackTracesOnSigQuit(appName)
+
+	app()
+}
+
 func Exit(errorMessage string) {
     fmt.Fprintln(os.Stderr, errorMessage)
     os.Exit(255)
