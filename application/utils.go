e0edeba816d760b3d69bfdf97225b27522d1ce95
maxim.manuylov <maxim.manuylov@gmail.com>
2017-01-27 14:47:24 +0300

    Run() method

--- application/utils.go
+++ application/utils.go
@@ -5,6 +5,12 @@ import (
     "os"
 )
 
+func Run(app func() error) {
+    if err := app(); err != nil {
+        Exit(err.Error())
+    }
+}
+
 func Exit(errorMessage string) {
     fmt.Fprintln(os.Stderr, errorMessage)
     os.Exit(255)
